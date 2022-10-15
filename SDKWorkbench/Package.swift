2b8606fcb54ab131b80d60bd295318780954e792
Josh Elkins <jbelkins@amazon.com>
2022-10-14 22:52:36 -0500

    Prep for development with 5.5

--- SDKWorkbench/Package.swift
+++ SDKWorkbench/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 
 import PackageDescription
 
@@ -7,7 +7,7 @@ let package = Package(
     name: "SDKWorkbench",
     platforms: [.macOS(.v10_15), .iOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", from: "0.2.6"),
+        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "jbe/to_be_main"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
     ],
     targets: [
