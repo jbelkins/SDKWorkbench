db2578e85ef8d169df7b3b336f59a2ee84808c33
Josh Elkins <jbelkins@amazon.com>
2022-10-17 11:24:20 -0500

    Add Vapor app target & tests

--- SDKWorkbench/Package.swift
+++ SDKWorkbench/Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "SDKWorkbench",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [.macOS(.v12), .iOS(.v15)],
     dependencies: [
         .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "jbe/to_be_main"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "CLITool", 
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")]
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+        ),
+        .target(
+            name: "VaporApp",
+            dependencies: [.product(name: "Vapor", package: "vapor")]
+        ),
+        .executableTarget(
+            name: "VaporRun",
+            dependencies: [.target(name: "VaporApp")]
         ),
         .testTarget(name: "CLIToolTests", dependencies: ["CLITool"]),
+        .testTarget(name: "VaporAppTests", dependencies: [.target(name: "VaporApp"), .product(name: "XCTVapor", package: "vapor")
+        ])
     ]
 )
---
+++ SDKWorkbench/Tests/VaporAppTests/VaporAppTests.swift
@@ -0,0 +1,15 @@
+@testable import VaporApp
+import XCTVapor
+
+final class VaporAppTests: XCTestCase {
+    func testHelloWorld() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "hello", afterResponse: { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertEqual(res.body.string, "Hello, world!")
+        })
+    }
+}
