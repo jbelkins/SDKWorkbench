42f95f24703b7dbf141d53001b18dc034bb2a9f8
Josh Elkins <jbelkins@amazon.com>
2022-11-10 09:56:57 -0600

    Lock to main, ignore Package.resolved

--- SDKWorkbench/Package.swift
+++ SDKWorkbench/Package.swift
@@ -7,14 +7,20 @@ let package = Package(
     name: "SDKWorkbench",
     platforms: [.macOS(.v12), .iOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "jbe/to_be_main"),
+        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "main"),
+        .package(url: "https://github.com/awslabs/smithy-swift.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "CLITool", 
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "AWSS3", package: "aws-sdk-swift"),
+                .product(name: "AWSSTS", package: "aws-sdk-swift"),
+                .product(name: "ClientRuntime", package: "smithy-swift"),
+            ]
         ),
         .target(
             name: "VaporApp",
@@ -27,5 +33,6 @@ let package = Package(
         .testTarget(name: "CLIToolTests", dependencies: ["CLITool"]),
         .testTarget(name: "VaporAppTests", dependencies: [.target(name: "VaporApp"), .product(name: "XCTVapor", package: "vapor")
         ])
-    ]
+    ],
+    swiftLanguageVersions: [.version("5.5")]
 )
