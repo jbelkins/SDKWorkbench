eae4bf47b2e7a4eb1430e82d6245dff62c398c3a
Josh Elkins <jbelkins@amazon.com>
2023-02-10 17:04:57 -0600

    Removed Vapor, local CRT copy

--- SDKWorkbench/Package.swift
+++ SDKWorkbench/Package.swift
@@ -7,31 +7,17 @@ let package = Package(
     name: "SDKWorkbench",
     platforms: [.macOS(.v12), .iOS(.v15)],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "main"),
-        .package(url: "https://github.com/awslabs/smithy-swift.git", branch: "main"),
+        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", from: "0.9.2"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "CLITool", 
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "AWSS3", package: "aws-sdk-swift"),
-                .product(name: "AWSSTS", package: "aws-sdk-swift"),
-                .product(name: "ClientRuntime", package: "smithy-swift"),
+                .product(name: "AWSSTS", package: "aws-sdk-swift")
             ]
         ),
-        .target(
-            name: "VaporApp",
-            dependencies: [.product(name: "Vapor", package: "vapor")]
-        ),
-        .executableTarget(
-            name: "VaporRun",
-            dependencies: [.target(name: "VaporApp")]
-        ),
         .testTarget(name: "CLIToolTests", dependencies: ["CLITool"]),
-        .testTarget(name: "VaporAppTests", dependencies: [.target(name: "VaporApp"), .product(name: "XCTVapor", package: "vapor")
-        ])
     ]
 )
--- SDKWorkbench/Sources/CLITool/CLIToolRun.swift
+++ SDKWorkbench/Sources/CLITool/CLIToolRun.swift
@@ -1,11 +1,22 @@
 import ArgumentParser
-
+import AWSSTS
 
 @main
 struct CLIToolRun: AsyncParsableCommand {
-    
+
     func run() async throws {
-        // Fill this in with whatever operations you want to run
-        print("Hello, world!")
+        let client = try STSClient(region: "us-east-1")
+        let input = GetCallerIdentityInput()
+        do {
+            let output = try await client.getCallerIdentity(input: input)
+            let userID = String(describing: output.userId)
+            let account = String(describing: output.account)
+            print("Request succeeded")
+            print("User ID: \(userID)")
+            print("Account: \(account)")
+        } catch {
+            print("Request failed")
+            print("Error: \(error)")
+        }
     }
 }
