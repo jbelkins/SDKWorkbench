611f4370d613be4176ec5c4deba8ed6a8334ae93
Josh Elkins <jbelkins@amazon.com>
2024-06-30 15:34:44 -0500

    Add schemes for separate protocol, integration tests

--- SDKWorkbench/Package.swift
+++ SDKWorkbench/Package.swift
@@ -1,14 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 
 import PackageDescription
-
 
 let package = Package(
     name: "SDKWorkbench",
     platforms: [.macOS(.v12), .iOS(.v15)],
     dependencies: [
         .package(url: "https://github.com/awslabs/aws-sdk-swift.git", branch: "main"),
-//        .package(url: "https://github.com/awslabs/smithy-swift.git", branch: "main"),
+        .package(url: "https://github.com/awslabs/smithy-swift.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
     ],
     targets: [
@@ -17,18 +16,8 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "AWSSTS", package: "aws-sdk-swift"),
-//                .product(name: "AWSRoute53", package: "aws-sdk-swift"),
-//                .product(name: "AWSS3", package: "aws-sdk-swift"),
-            ],
-            linkerSettings: [
-                .unsafeFlags(
-                    ["-Xlinker", "-sectcreate",
-                     "-Xlinker", "__TEXT",
-                     "-Xlinker", "__info_plist",
-                     "-Xlinker", "Resources/Info.plist"
-                    ]
-                )
             ]
-        )
+        ),
+        .testTarget(name: "CLIToolTests", dependencies: ["CLITool"]),
     ]
 )
--- SDKWorkbench/Sources/CLITool/CLIToolRun.swift
+++ SDKWorkbench/Sources/CLITool/CLIToolRun.swift
@@ -1,16 +1,11 @@
 import ArgumentParser
-import Foundation
 import AWSSTS
-import AWSClientRuntime
-//import AWSRoute53
 
 @main
 struct CLIToolRun: AsyncParsableCommand {
 
     func run() async throws {
-        let credentials = 
-        let config = STSClient.Config(awsCredentialIdentityResolver: StaticAWSCredentialIdentityResolver(<#AWSCredentialIdentity#>)
-        let client = try STSClient(config: config)
+        let client = try STSClient(region: "us-east-1")
         let input = GetCallerIdentityInput()
         do {
             let output = try await client.getCallerIdentity(input: input)
@@ -24,24 +19,4 @@ struct CLIToolRun: AsyncParsableCommand {
             print("Error: \(error)")
         }
     }
-
-//    func run() async throws {
-//        let client = try S3Client(region: "us-west-2")
-//        let input = ListBucketsInput()
-//        let output = try await client.listBuckets(input: input)
-//        for bucket in output.buckets ?? [] where bucket.name?.hasPrefix("aws-sdk-s3-integration-test-") ?? false {
-//            let input = ListObjectsV2Input(bucket: bucket.name)
-//            let output = try await client.listObjectsV2(input: input)
-//            for object in output.contents ?? [] {
-//                let input = DeleteObjectInput(bucket: bucket.name, key: object.key)
-//                let _ = try await client.deleteObject(input: input)
-//            }
-//            let input0 = DeleteBucketInput(bucket: bucket.name)
-//            do {
-//                let _ = try await client.deleteBucket(input: input0)
-//            } catch let error as AWSServiceError where error.errorCode == "BucketHasAccessPointsAttached" {
-//                // Ignore this error & leave this bucket
-//            }
-//        }
-//    }
 }
